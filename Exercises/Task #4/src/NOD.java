776d8d513afaf86fdfb4becd411e89bbb5fa8e4a
unknown <lruzy@ya.ru>
2020-06-07 20:12:12 +0700

    Задание 4. Поиск НОД

---
+++ Exercises/Task #4/src/NOD.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class NOD {
+    public static void main(String[] args) {
+
+        System.out.println("Введите первое число");
+        int number1 = (new Scanner(System.in)).nextInt();
+        System.out.println("Введите второе число");
+        int number2 = (new Scanner(System.in)).nextInt();
+
+        int nod = 1;
+        int nod2 = 1;
+        int numberA = 1;
+        int numberB = 1;
+
+        if (number1 > number2) {
+            numberA = number1;
+            numberB = number2;
+        }
+        if (number2 > number1) {
+            numberA = number2;
+            numberB = number1;
+        }
+
+
+        for (; numberA >= 0; numberA -= nod2) {
+            if (numberA > numberB)
+                nod = numberA - numberB;
+            nod2 = numberB;
+            if (nod2 > nod)
+                nod = nod2 - nod;
+        }
+        {
+            if (number1 % nod == 0 && number2 % nod == 0) {
+                System.out.println("НОД = " + nod);}
+            else {
+                System.out.println("НОД = 1");
+            }
+
+        }
+    }
+}
+
+
+
+
---
+++ Exercises/Task #4/src/NOD2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class NOD2 {
+        public static void main(String[] args) {
+
+            System.out.println("Введите первое число");
+            int number1 = (new Scanner(System.in)).nextInt();
+            System.out.println("Введите второе число");
+            int number2 = (new Scanner(System.in)).nextInt();
+            int number = 1;
+            if (number1 > number2) {
+                number = number1; }
+            if (number2 > number1) {
+                number =number2;
+            }
+
+            for (; number > 1; number--) { //почему он доходит до 1, если в условии number > 1, а не >=1
+                if (number1 % number == 0 && number2 % number == 0)
+                    break;
+            }
+            System.out.println("НОД = " + number);
+        }
+    }
+
