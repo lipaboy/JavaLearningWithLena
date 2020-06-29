dada49a138cdc5ae395d9f69e925de98744b1709
elenabalakhtar <elenabalakhtar@gmail.com>
2020-06-29 22:16:31 +0700

    Грузовик сделан

--- Exercises/IntegerExperiments/src/Gruzovik.java
+++ Exercises/IntegerExperiments/src/Gruzovik.java
@@ -6,39 +6,33 @@ public class Gruzovik {
 
 
     public static void main(String[] args) {
+        System.out.println("Введите количество ящиков: ");
         int boxAmount = new Scanner(System.in).nextInt();
-
-        int kolvoPolniyKonteinerov = boxAmount / MAX_KOLICHESTVO_YASHIKOV;
-        int kilvoReal;
-        if (boxAmount % 27 != 0) {
-            kilvoReal = kolvoPolniyKonteinerov + 1;
-        } else {
-            kilvoReal = kolvoPolniyKonteinerov;
-        }
-
 
         int yashFirst = 1;
         int yashLast = 27;
         int kontFirst = 1;
         int kontLast = 12;
+        int yashik = 1;
 
-        int gruzPolniyKolvo = kilvoReal / 12;
+        double gruzLast = (int) Math.ceil((double) boxAmount / MAX_KOLICHESTVO_CONTEINEROV / MAX_KOLICHESTVO_YASHIKOV);
 
-        // заполняем полные грузовики контейнерами
-
-        for (int gruz = 1; gruz <= gruzPolniyKolvo; gruz++) {
-            if (gruz == gruzPolniyKolvo + 1) {
+        for (int gruz = 1; gruz <= gruzLast; gruz++) {
+            if (yashik == boxAmount + 1)
                 break;
-            }
             System.out.println("Грузовик: " + gruz);
 
-            // заполняем контейнеры ящиками до конца. Здесь не учитывается неполный контейнер
-
             for (int kont = kontFirst; kont <= kontLast; kont++) {
-                System.out.println("Контейнер: " + kont);
+                if (yashik == boxAmount + 1)
+                    break;
+                System.out.println("\tКонтейнер: " + kont);
+
                 // container #kont
-                for (int yash = yashFirst; yash <= yashLast; yash++) {
-                    System.out.println("\tЯщик: " + yash);
+                for (yashik = yashFirst; yashik <= yashLast; yashik++) {
+                    if (yashik == boxAmount + 1) {
+                        break;
+                    }
+                    System.out.println("\t\tЯщик: " + yashik);
 
                 }
 
@@ -50,52 +44,10 @@ public class Gruzovik {
             kontFirst = kontLast + 1;
             kontLast = kontFirst + 11;
 
-
-            // остаток по ящикам засовываем в остаточный контейнер
-
-            int yashLast2 = yashLast;
-            yashLast = boxAmount - (yashLast - 26);
-            if (yashLast < 27)
-            {
-            int gruz2 = gruz + 1;
-            System.out.println("Грузовик: " + gruz2);
-            System.out.println("Контейнер: " + kolvoPolniyKonteinerov);
-            for (int yash = yashFirst; yash <= boxAmount; yash++)
-                {
-                System.out.println("\tЯщик: " + yash);
-                }
-
-            }
-
-            // обрабатываем случай, когда грузовик не заполнен контейнерами до конца
-
-            if (kolvoPolniyKonteinerov % 12 !=0 ) {
-                yashLast = yashLast2;
-                gruz = gruzPolniyKolvo+1;
-                System.out.println("Грузовик: " + gruz);
-                for (int kont = kontFirst; kont <= kolvoPolniyKonteinerov; kont++) {
-                    System.out.println("Контейнер: " + kont);
-                    // container #kont
-                    for (int yash = yashFirst; yash <= yashLast; yash++) {
-                        System.out.println("\tЯщик: " + yash);
-
-                    }
-                    yashFirst = yashLast + 1;
-                    yashLast = yashFirst + 26;
-                }
-                if (boxAmount % 27 != 0) {
-                    yashLast = boxAmount;
-                    int kolvo = kolvoPolniyKonteinerov + 1;
-                    System.out.println("Контейнер: " + kolvo);
-                    for (int yash = yashFirst; yash <= yashLast; yash++) {
-                        System.out.println("\tЯщик: " + yash);
-                    }
-
-                }
-            }
         }
     }
 }
+
 
 
 
